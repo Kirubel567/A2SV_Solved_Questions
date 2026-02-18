a43d57752b1d6a7f8ced720bb3f4c3d9b651fa98
Kirubel Wubet <kirubel.wubet1996@gmail.com>
2026-02-18 23:11:41 +0300

    Time: 743 ms (5.15%), Space: 19.6 MB (66.85%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,16 +1,13 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        mapp = {}
-        ans = []
         for i in range(len(heights)): 
-            mapp[heights[i]] = names[i]
-
-        for i in range(len(heights)): 
-            for j in range(i+1, len(heights)): 
-                if heights[i] < heights[j]: 
-                    heights[i], heights[j] = heights[j], heights[i]
-        print(heights)
-        for h in heights: 
-            ans.append(mapp[h])
+            swapped = False
+            for j in range(len(heights)-i-1): 
+                if heights[j] < heights[j+1]: 
+                    heights[j], heights[j+1] = heights[j+1], heights[j]
+                    names[j], names[j+1] = names[j+1], names[j]
+                    swapped = True 
+            if not swapped: 
+                break 
         
-        return ans 
+        return names 
