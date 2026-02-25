e9ca0ae2061281f9e4c5a1bba9717a44a947f386
Kirubel Wubet <kirubel.wubet1996@gmail.com>
2026-02-25 18:08:10 +0300

    Time: 11 ms (6.95%), Space: 19.7 MB (22.1%) - LeetHub

---
+++ 0763-partition-labels/0763-partition-labels.py
@@ -0,0 +1,24 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        #have a last index holder of each characters 
+        last = {}
+        for i in range(len(s)): 
+            last[s[i]] = i
+    
+        #iterate over the array and determine where the last of the character is found, then while iterating through the current last index, check if the next elements last is after the firsts
+        #eg: abab a's last is 2 up until 2 check if b's last is greater than this one if it's greater then take that range
+        #make the maximum as the end, and if we reach the end(the max index) add the size there 
+        length, end = 0, -float('inf')
+        ans = []
+        for i in range(len(s)): 
+            end = max(end, last[s[i]]) #current max
+            length += 1
+            print(length)
+
+            if i == end: #we reached 
+                ans.append(length)
+                length = 0
+        return ans 
+
+
+
