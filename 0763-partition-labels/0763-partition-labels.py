ac1bc8baef414372e6a371e4fe84d22b9792c225
Kirubel Wubet <kirubel.wubet1996@gmail.com>
2026-02-25 18:13:12 +0300

    Time: 4 ms (45.04%), Space: 19.4 MB (22.1%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.py
+++ 0763-partition-labels/0763-partition-labels.py
@@ -8,16 +8,13 @@ class Solution:
         #iterate over the array and determine where the last of the character is found, then while iterating through the current last index, check if the next elements last is after the firsts
         #eg: abab a's last is 2 up until 2 check if b's last is greater than this one if it's greater then take that range
         #make the maximum as the end, and if we reach the end(the max index) add the size there 
-        length, end = 0, -float('inf')
+        start, end = 0, -float('inf')
         ans = []
         for i in range(len(s)): 
             end = max(end, last[s[i]]) #current max
-            length += 1
-            print(length)
-
             if i == end: #we reached 
-                ans.append(length)
-                length = 0
+                ans.append(end-start+1)
+                start = i+1
         return ans 
 
 
