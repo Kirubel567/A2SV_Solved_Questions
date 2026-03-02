86d1337794227709014c140a69ccd7e40c5c018a
Kirubel Wubet <kirubel.wubet1996@gmail.com>
2026-03-02 20:44:33 +0300

    Time: 0 ms (100%), Space: 19.2 MB (75.47%) - LeetHub

--- 3432-count-partitions-with-even-sum-difference/3432-count-partitions-with-even-sum-difference.py
+++ 3432-count-partitions-with-even-sum-difference/3432-count-partitions-with-even-sum-difference.py
@@ -1,20 +1,11 @@
 class Solution:
     def countPartitions(self, nums: List[int]) -> int:
-        #create a inclusive pre_fix and exclusive post_fix 
-        pre_fix = []
-        accumulate = 0
-        for i in range(len(nums)):
-            accumulate += nums[i]
-            pre_fix.append(accumulate)
+        total_sum = sum(nums)
 
         accumulate, count = 0, 0
-        post_fix = [0]*len(nums)
-        for i in range(len(nums)-1, -1, -1): 
-            post_fix[i] = accumulate
-            accumulate += nums[i]
-
         for i in range(len(nums)-1): 
-            if (pre_fix[i] - post_fix[i])%2==0: 
+            accumulate += nums[i]
+            if (accumulate - (total_sum - accumulate))%2==0: 
                 count += 1
-
-        return count
+            
+        return count 
