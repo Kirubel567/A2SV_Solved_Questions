8369bdd8360d20f923d295e5a34f1893d3d36310
Kirubel Wubet <kirubel.wubet1996@gmail.com>
2026-03-13 11:39:14 +0300

    Add solution for Sliding Window Maximum

--- leetcode/0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ leetcode/0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -1,20 +1,14 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
         _max = deque()
-        ans = []
-        for i in range(k): 
-            while _max and _max[-1]<nums[i]: 
-                _max.pop()
-            _max.append(nums[i])
-        ans.append(_max[0])
-        
-        for r in range(k, len(nums)): 
+        ans = [] 
+        for r in range(len(nums)): 
             while _max and _max[-1]<nums[r]:
                 _max.pop()
             _max.append(nums[r])
 
-            if nums[r-k] == _max[0]: 
+            if r >= k and nums[r-k] == _max[0]: 
                 _max.popleft()
-
-            ans.append(_max[0])
+            if r >= k-1: 
+                ans.append(_max[0])
         return ans 
