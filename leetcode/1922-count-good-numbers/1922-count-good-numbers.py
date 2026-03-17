87a001525c3fb2ee0377179ecc8b1d85c0e791fb
Kirubel Wubet <kirubel.wubet1996@gmail.com>
2026-03-17 19:22:24 +0300

    Add solution for Count Good Numbers

--- leetcode/1922-count-good-numbers/1922-count-good-numbers.py
+++ leetcode/1922-count-good-numbers/1922-count-good-numbers.py
@@ -1,13 +1,15 @@
 class Solution:
-    def countGoodNumbers(self, n: int) -> int:
-        #even numbers 0, 2, 4, 6, 8
-        #prime numbers 2,3,5,7,
-
-        #calculation if evevn half*half
-        #calculation if odd half*half * 5
-
+    def power(self, x: int, n: int) -> float:
+        if n == 0: 
+            return 1
+        half = self.power(x, n//2)
         if n %2==0: 
-            return (pow(5,(n//2), (1000000000+7)) * pow(4,(n//2), (1000000000+7)))%(1000000000+ 7)
-        else: 
-            return (pow(5,((n-1)//2), (1000000000+7))* pow(4,((n-1)//2), (1000000000+7)) *5)%(1000000000+7)
+            return (half*half)%(10**9 + 7)
+    
+        return (x*half*half)%(10**9 + 7)
 
+    def countGoodNumbers(self, n: int) -> int:
+        prime = n//2
+        even = n//2 if n%2==0 else (n+1)//2 
+    
+        return (self.power(5, even) *self.power(4,prime))%(1000000000+7)
