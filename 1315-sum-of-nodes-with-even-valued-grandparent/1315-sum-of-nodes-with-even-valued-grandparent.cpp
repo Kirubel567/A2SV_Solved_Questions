b55ac1a0ea534ed9af582cf3acf110f775f9105a
Kirubel Wubet <kirubel.wubet1996@gmail.com>
2026-03-21 22:31:37 +0300

    Time: 0 ms (100%), Space: 42.1 MB (34.31%) - LeetHub

---
+++ 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.cpp
@@ -0,0 +1,16 @@
+
+class Solution {
+public:
+    int sum=0;
+    void traverse(TreeNode* root,TreeNode* parent,TreeNode* grandpa){
+        if(!root)return ;
+        if(grandpa and grandpa->val%2==0)sum+=root->val;
+        if(parent)grandpa=parent;
+        traverse(root->left,root,grandpa);
+        traverse(root->right,root,grandpa);
+    }
+    int sumEvenGrandparent(TreeNode* root) {
+        traverse(root,nullptr,nullptr);
+        return sum;
+    }
+};
