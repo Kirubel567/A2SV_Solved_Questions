3bef27e35a6292d3730b462d88c406ec9639b9ed
Kirubel Wubet <kirubel.wubet1996@gmail.com>
2026-04-23 00:20:52 +0300

    Add solution for N-Queens II

--- leetcode/0052-n-queens-ii/0052-n-queens-ii.py
+++ leetcode/0052-n-queens-ii/0052-n-queens-ii.py
@@ -1,34 +1,29 @@
 class Solution:
     def totalNQueens(self, n: int) -> int:
-        board=[['.' for _ in range(n)] for _ in range(n)]
-        path = []
-        count = 0
+        board = [["." for _ in range(n)] for _ in range(n)]
 
-
-        def backtrack(row):
-            nonlocal count
+        counter=0
+        def backtrack(row): 
+            nonlocal counter
             if row == n: 
-                count+=1
+                counter += 1
                 return 
             
             for col in range(n): 
-                if isSafe(row, col): 
+                if validate(row, col): 
                     board[row][col] = "Q"
                     backtrack(row+1)
                     board[row][col] = "."
-                
-        def isSafe(row, col): 
-            for i in range(row): 
-                if board[i][col] == "Q": 
+        def validate(row, col): 
+            for r in range(row): 
+                if board[r][col] == "Q": 
                     return False 
-
             for i in range(1, min(row, col)+1): 
                 if board[row-i][col-i] == "Q": 
                     return False 
-            for i in range(1, min(row, n-1-col)+1): 
-                if board[row-i][col+i] == "Q":
-                    return False 
-                
+            for i in range(1, min(row, n-col-1)+1): 
+                if board[row-i][col+i] == "Q": 
+                    return False
             return True 
         backtrack(0)
-        return count 
+        return counter
