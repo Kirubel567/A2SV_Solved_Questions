793006eef4a7ff0dc5c5ee1d02b72258b1a9b9fd
Kirubel Wubet <kirubel.wubet1996@gmail.com>
2026-04-23 00:06:06 +0300

    Add solution for Letter Combinations of a Phone Number

--- leetcode/0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ leetcode/0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,21 +1,23 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        check={'1': "", '2': "abc", '3': "def", '4': "ghi", '5': "jkl", '6': "mno", '7': "pqrs", '8': "tuv", '9': "wxyz"}
-        path = []
-        ans=[]
         n = len(digits)
-        def backtrack(idx): 
-            if len(path) == n: 
-                ans.append("".join(path.copy()))
-                return  
+        mapp = {'1': "", '2': "abc", '3': "def", '4': "ghi", '5': "jkl", '6': "mno", '7': "pqrs", '8': "tuv", '9': "wxyz"}
 
-            st = check[digits[idx]]
-            for j in range(len(st)):
-                path.append(st[j])
-                backtrack(idx+1)
+        #use backtracking, just select all the possible combinations from the starting letter to the rest of the letters, the base case is when the len(path) == len(digits), return here by adding it to the answer 
+        ans =[]
+        path =[]
+
+        def backtrack(i): 
+            if len(path) == len(digits): 
+                ans.append(''.join(path))
+                return 
+            if i >= len(digits): 
+                return 
+            
+            for letter in mapp[digits[i]]: 
+                path.append(letter)
+                backtrack(i+1)
                 path.pop()
-
+            
         backtrack(0)
-        return ans  
-
-
+        return ans 
