ce032cbe2b57ee7fc9692212c727160456df18f9
Kirubel Wubet <kirubel.wubet1996@gmail.com>
2026-04-23 20:18:09 +0300

    Add solution for Permutations

--- leetcode/0046-permutations/0046-permutations.py
+++ leetcode/0046-permutations/0046-permutations.py
@@ -1,17 +1,16 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
+        path =[]
         ans = []
-        path = []
-        def backtrack(candidates):
-            if not candidates: 
+        def backtrack(candidates): 
+            if len(path) == len(nums): 
                 ans.append(path.copy())
+                return
 
             for i in range(len(candidates)): 
-                path.append(candidates[i]) 
-
+                path.append(candidates[i])
                 backtrack(candidates[:i]+candidates[i+1:])
-
                 path.pop()
+        
         backtrack(nums)
         return ans 
-        
