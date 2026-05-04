75bbb14759ef4529f15763117c399e81f3bca62f
Kirubel Wubet <kirubel.wubet1996@gmail.com>
2026-05-04 20:05:47 +0300

    Add solution for Dreamoon and WiFi

--- codeforces/476B-Dreamoon-and-WiFi/476B-Dreamoon-and-WiFi.py
+++ codeforces/476B-Dreamoon-and-WiFi/476B-Dreamoon-and-WiFi.py
@@ -1,30 +1,59 @@
-from math import comb
+#first get all the possibilities 
+#for each levels where pos[i] == "?" check both possibilities by making it + or -
+#after building all the possibilities array compare how many of them are the same result as the right ones 
 
-s1 = input().strip()
-s2 = input().strip()
 
-target = s1.count('+') - s1.count('-')
+#move from left to right, if the curr_idx >= len(recieved) then add count to the possibilities 
 
-current = 0
-q = 0
+target_s = list(input().strip())
+target = 0
+for ch in target_s: 
+    if ch == "+": 
+        target += 1
+    else: 
+        target -= 1
 
-for c in s2:
-    if c == '+':
-        current += 1
-    elif c == '-':
-        current -= 1
-    else:
-        q += 1
+recieved = list(input().strip())
+possibilities = []
 
-diff = target - current
+def dfs(curr_idx, count, recieved): 
+    if curr_idx >= len(recieved): 
+        possibilities.append(count)
+        count = 0
+        return 
+    
+    #check each characters 
+    ch = recieved[curr_idx]
 
-if (diff + q) % 2 != 0:
-    print("0.000000000000")
-else:
-    x = (diff + q) // 2
-    if x < 0 or x > q:
-        print("0.000000000000")
-    else:
-        ways = comb(q, x) 
-        total = 2 ** q
-        print(f"{ways / total:.12f}")
+    #if ch == "+" add one to count
+    #if ch == "-" subtract one from count 
+    #else try two different ways 
+    #make the "?" "+" and call the dfs
+    #or make the "?" "-" and call the dfs 
+
+    if ch == "+": 
+        dfs(curr_idx+1, count+1, recieved)
+    elif ch == "-":
+        dfs(curr_idx+1, count-1, recieved)
+    else: 
+        #make "?" a "+"
+        curr_str = recieved[:]
+
+        curr_str[curr_idx] = "+"
+        dfs(curr_idx+1, count+1, curr_str)
+        curr_str[curr_idx] = "?"
+
+        #make "?" a "-"
+        curr_str[curr_idx] = "-"
+        dfs(curr_idx+1, count-1, curr_str)
+        curr_str[curr_idx] = "?"
+
+dfs(0, 0, recieved)
+total = len(possibilities)
+right_pos = 0
+
+for num in possibilities: 
+    if target == num: 
+        right_pos+=1
+
+print("%.12f" % (right_pos / total))
